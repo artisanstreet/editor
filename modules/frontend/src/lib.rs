99b4d1924384f9935b299de7f5fe91c6aaa8bdfe
ss <sander@sonstabo.com>
2026-08-28 19:30:04 +0200

    build(frontend): register host resume recovery policy

--- modules/frontend/src/lib.rs
+++ modules/frontend/src/lib.rs
@@ -44,6 +44,7 @@ pub mod engine_usage_cache;
 pub mod file_icon;
 pub mod forge_recovery_health;
 pub mod harness_setup_policy;
+pub mod host_resume_recovery_policy;
 pub mod hover_pill_geometry_policy;
 pub mod hover_pill_group_policy;
 pub mod image_inspection_store;
