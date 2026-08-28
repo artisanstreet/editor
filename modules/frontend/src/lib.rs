97f2f31d7359104920498edd928d12e4cdd9d9b0
ss <sander@sonstabo.com>
2026-08-28 04:42:26 +0200

    build(frontend): register conversation presentation

--- modules/frontend/src/lib.rs
+++ modules/frontend/src/lib.rs
@@ -12,6 +12,7 @@ pub mod command_ranking;
 pub mod composer;
 pub mod context_usage_tone;
 pub mod conversation_diff_stat;
+pub mod conversation_presentation;
 pub mod conversation_projection;
 pub mod image_viewer;
 pub mod project_picker;
